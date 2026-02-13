f8050c329e4bdd30dce467efa385e62f6b8397c4
ibazzam <ibrahim.azzam@gmail.com>
2026-02-13 18:26:10 +0800

    Add test line to index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
+test
+    
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
